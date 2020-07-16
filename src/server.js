7b4dc3dd5d21fd8556709de2e9b07be5caf40cc1
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2020-07-15 21:56:43 -0400

    Implementando el cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
+const cors = require('cors');
 const app = express();
 
 app.use(morgan('dev'));
+
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
