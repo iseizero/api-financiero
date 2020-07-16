dde51ffc7756537ce9ec872c33750bf5221be79e
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2020-07-15 21:55:22 -0400

    Resto de endpoints

--- src/components/stadistics/stadistics.controller.js
+++ src/components/stadistics/stadistics.controller.js
@@ -3,8 +3,30 @@ const BASE_URL = 'https://www.indecon.online/';
 
 exports.getAllEndpoints = async (req, res) => {
     axios.get(`${BASE_URL}last`).then((result) => {
-        console.log(result.data);
         return res.send({result: result.data}).status(200);
+    }).catch((err) => {
+        return res.status(500).send({
+            message: err.message
+        });
+    });
+}
+
+exports.getValuesByKey = (req, res) => {
+    var key = req.params.key;
+    axios.get(`${BASE_URL}/values/${key}`).then((result) => {
+        return res.send(result.data).status(200);
+    }).catch((err) => {
+        return res.status(500).send({
+            message: err.message
+        });
+    });
+}
+
+exports.getValuesByDateAndDate = (req,res) => {
+    var { key , date } = req.params;
+    
+    axios.get(`${BASE_URL}/date/${key}/${date}`).then((result) => {
+        return res.send(result.data).status(200);
     }).catch((err) => {
         return res.status(500).send({
             message: err.message
--- src/components/stadistics/stadistics.route.js
+++ src/components/stadistics/stadistics.route.js
@@ -5,5 +5,7 @@ const stadistics_controller = require('./stadistics.controller');
 context = 'numbers';
 
 router.get(`/${context}/all`, stadistics_controller.getAllEndpoints);
+router.get(`/${context}/values/:key`, stadistics_controller.getValuesByKey);
+router.get(`/${context}/date/:key/:date`, stadistics_controller.getValuesByDateAndDate);
 
 module.exports = router;
