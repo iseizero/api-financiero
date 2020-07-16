717a68e660fb83fae2fad0cb37e7b5d68b0f887e
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2020-07-16 00:51:01 -0400

    Implementando moment para ordenar por fecha

--- src/components/stadistics/stadistics.controller.js
+++ src/components/stadistics/stadistics.controller.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const moment = require('moment');
 const BASE_URL = 'https://www.indecon.online/';
 
 exports.getAllEndpoints = async (req, res) => {
@@ -15,13 +16,20 @@ exports.getAllEndpoints = async (req, res) => {
 exports.getValuesByKey = (req, res) => {
     var key = req.params.key;
     axios.get(`${BASE_URL}/values/${key}`).then((result) => {
-        return res.send(result.data).status(200);
+        var data = {
+            key: result.data.key,
+            name: result.data.name,
+            unit: result.data.unit,
+            values: transformValuesData(result.data.values)
+        }
+        return res.send(data).status(200);
     }).catch((err) => {
         return res.status(500).send({
             message: err.message
         });
     });
 }
+
 
 exports.getValuesByDateAndDate = (req,res) => {
     var { key , date } = req.params;
@@ -33,4 +41,12 @@ exports.getValuesByDateAndDate = (req,res) => {
             message: err.message
         });
     });
+}
+
+transformValuesData = (values) => {
+    var arrayDate = Object.entries(values).map((a) => {
+        return {date: a[0], value: a[1]}
+    });
+
+    return arrayDate;
 }
