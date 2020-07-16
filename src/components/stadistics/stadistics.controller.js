1b4a976c8e02ac0b47ed37284794b81f128563ee
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2020-07-15 23:58:28 -0400

    transformando objeto

--- src/components/stadistics/stadistics.controller.js
+++ src/components/stadistics/stadistics.controller.js
@@ -3,7 +3,8 @@ const BASE_URL = 'https://www.indecon.online/';
 
 exports.getAllEndpoints = async (req, res) => {
     axios.get(`${BASE_URL}last`).then((result) => {
-        return res.send({result: result.data}).status(200);
+        var auxResult = Object.values(result.data);
+        return res.send(auxResult).status(200);
     }).catch((err) => {
         return res.status(500).send({
             message: err.message
